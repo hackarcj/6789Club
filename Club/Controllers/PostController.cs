bda8ae13b1a33e235447d3c212c3525c58b33c87
hackar_cj <1501339871@qq.com>
2017-10-13 13:57:10 +0800

    帖子发布

--- Club/Controllers/PostController.cs
+++ Club/Controllers/PostController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace Club.Controllers
 {
-    public class PostController : Controller
+    public class PostController : BaseController
     {
         // GET: Post
         public ActionResult Index()
@@ -16,6 +16,39 @@ namespace Club.Controllers
         public ActionResult Search()
         {
             return View();
+        }
+        [UserCheck(IsNeed = true)]
+        public ActionResult New()
+        {
+            using (var db=new ClubEntities())
+            {
+                var type = db.Type.ToList();
+                ViewBag.type = type;
+            }
+            return View();
+        }
+        [UserCheck(IsNeed = true)]
+        [ValidateInput(false)]
+        [HttpPost]
+        public ActionResult Save()
+        {
+            var typeId = Request["typeId"].ToInt();
+            var title = Request["title"];
+            var content = Request["content"];
+            var loginUser = (User)Session["loginuser"];
+            using (var db=new ClubEntities())
+            {
+                var post = new Post();
+                post.Title = title;
+                post.Contents = content;
+                post.Userid = loginUser.id;
+                post.Typeid = typeId;
+                post.System = "Win10";
+                post.Time = DateTime.Now;
+                db.Post.Add(post);
+                db.SaveChanges();
+            }            
+            return Redirect("/");
         }
     }
 }
